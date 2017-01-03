ab476df76244a50f2f15bf8ff311d646ccee4638
Ricardo D. Quiroga <l2radamanthys@gamil.com>
2017-01-03 16:49:54 -0300

    trabajando en el capitulo 6 de ruteo

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,25 +1,35 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent }  from './hero-detail.component';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports:      [ 
     BrowserModule,
-    FormsModule 
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [ 
     AppComponent,
-    HeroDetailComponent 
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent,
   ],
-  bootstrap: [ 
-    AppComponent 
-  ]
+  providers: [HeroService],
+  bootstrap: [AppComponent],
 })
+
 
 export class AppModule {
 
 
 }
+
+
