767655f881f35597160adb81334a6acd7e6a17da
Ricardo D. Quiroga <l2radamanthys@gamil.com>
2017-01-03 15:41:45 -0300

    capitulo 4 completo

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,8 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -78,15 +67,29 @@ const HEROES: Hero[] = [
       border-radius: 4px 0 0 4px;
     }
   `],
+  providers: [HeroService],
 })
 
-export class AppComponent  { 
+export class AppComponent implements OnInit { 
+  constructor(private heroService: HeroService) { }
+
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(
+      heroes => this.heroes = heroes
+    );
+    
   }
 }
 
